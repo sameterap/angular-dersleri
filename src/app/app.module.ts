bde574c33f9c8c1733ff8d61fa695d7701e7f041
Samet Erap <samet.erap@live.com>
2019-12-25 16:12:02 +0300

    reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -43,6 +43,7 @@ import { PaginationModule } from 'ngx-bootstrap';
    imports: [
       BrowserModule,
       FormsModule,
+      ReactiveFormsModule,
       HttpClientModule,
       PaginationModule.forRoot(),
       BrowserAnimationsModule,
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { debounceTime, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userForm: FormGroup;
+  private destroySubject = new Subject<void>();
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.prepareForm();
+    this.listenChanges();
+  }
+
+  prepareForm() {
+    this.userForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', Validators.required],
+      confirmpassword: ['', Validators.required],
+      address: this.fb.group({
+        street: [''],
+        city: ['']
+      }),
+      additional: this.fb.array([
+        this.fb.control('')
+      ])
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  get additionalArray() {
+    return this.userForm.get('additional') as FormArray;
+  }
+
+
+  addAdditional() {
+    this.additionalArray.push(this.fb.control(''));
+  }
+
+  passwordMatchValidator(formGroup: FormGroup): any {
+    return formGroup.get('password').value === formGroup.get('confirmpassword').value ? null : { 'mismatch': true };
+  }
+
+  listenChanges() {
+    this.userForm.valueChanges
+      .pipe(
+        debounceTime(1000),
+        takeUntil(this.destroySubject))
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
+  sampleFetchedData(): any {
+    const data = { firstName: "Samet", lastName: "Erap", password: "1234", confirmpassword: "1234", address: { street: "yok", city: "yok" }, additional: [""] };
+
+    return data;
+  }
+
+  getData() {
+    this.userForm.patchValue(this.sampleFetchedData(), { emitEvent: false });
+  }
+
+  clearData() {
+    this.prepareForm();
+    // this.userForm.reset({emitEvent: false});
+  }
+
+  ngOnDestroy() {
+    this.destroySubject.next();
+    this.destroySubject.complete();
   }
 
 }
